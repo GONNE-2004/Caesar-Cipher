ab0310ff24ee2dfa2f53358d722e3e317e95f6dc
GONNE <gonne411@gmail.com>
2025-08-04 07:52:33 +0200

    added doc strings, put the main logic in to a function

---
+++ README.md
@@ -0,0 +1,21 @@
+PROJECT: CAESAR CIPHER
+
+DESCRIPTION:
+- A simple program that encrypts and decrypts messages.
+
+FEATURES:
+-Uses random module to import from another file
+-Implements the codes logic in a function
+-Shows the use of lower,isupper and upper function respectively
+-Good use of input validation and the try-except statement
+-Use of keyword arguments in functions
+-Use of modulo (%) with lists
+
+
+TOOLS:
+- Python 3.x
+- No external libraries required (uses `random` and standard modules).
+
+CODE STRUCTURE:
+- 'main.py': main logic
+- 'art.py' : ASCII art for caesar cipher
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 
 def caesar(original_text, shift_amount, encode_or_decode):
+    """holds the logic behind the encrypting and decrypting"""
     output_text = ""
     if encode_or_decode == "decode":
         shift_amount *= -1 #force the shift backwards
@@ -23,40 +24,48 @@ def caesar(original_text, shift_amount, encode_or_decode):
     print(f"Here is the {encode_or_decode}d result: {output_text}")
             #upper() converts lowercase to uppercase letters
             #isupper() checks if the letter is uppercase only so it's like true or false(boolean)
-should_continue = True
 
-while should_continue:
-    while True:
-          direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-          if direction in ["encode", "decode"]:
-              break
-          print("Invalid choice. Please enter 'encode' or 'decode'.")
-          # the while loop is very useful in the upper validation because
-          #  it allows the user more chances to input something from the list instead of a situation
-          # where it's not used then if the user chooses wrongly the program crashes
+def main():
+    """holds the main logic"""
+    should_continue = True
+    while should_continue:
+        while True:
+            direction = input("Type 'encode' to encrypt, type 'decode' to decrypt, or 'Q' to exit!\n").lower()
+            if direction in ["encode", "decode"]:
+                break
+            print("Invalid choice. Please enter 'encode' or 'decode'.")
+            # the while loop is very useful in the upper validation because
+            #  it allows the user more chances to input something from the list instead of a situation
+            # where it's not used then if the user chooses wrongly the program crashes
 
-    text = input("Type your message:\n")
+        text = input("Type your message:\n")
 
-    while True:
-         shift_input = input("Type the shift number:\n")
-         try:
-             shift = int(shift_input)
-             break
-         except ValueError:
-             print("invalid input. Please enter a digit number")
-# i could've just used this: shift = int(input(please enter a digit number)) in the upper codes which is shorter
-# but what if the user enters five or 3.14 the program would've crashed due to invalid input but
-# the try except saves the situation which when added with while loop prompts the user to try until they get it right,
-# though both code handle negative and positive integers well due to modulo % used in the caesar function
+        while True:
+            shift_input = input("Type the shift number:\n")
+            try:
+                shift = int(shift_input)
+                break
+            except ValueError:
+                print("invalid input. Please enter a digit number")
+    # i could've just used this: shift = int(input(please enter a digit number)) in the upper codes which is shorter
+    # but what if the user enters five or 3.14 the program would've crashed due to invalid input but
+    # the try except saves the situation which when added with while loop prompts the user to try until they get it right,
+    # though both code handle negative and positive integers well due to modulo % used in the caesar function
 
 
-    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+        caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
-    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
-    if restart == "no":
-        should_continue = False
-        print("Goodbye")
-    elif restart == "yes":
-        print("\n" * 20)
+        restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+        if restart == "no":
+            should_continue = False
+            print("Goodbye")
+        elif restart == "yes":
+            print("\n" * 20)
+
+
+
+
+
+main()
 
 
